ef037b7e5832cafcb02bc0ec6d52c1f822e3dcac
Chris Carrel <ccarrel@virtualhaven.net>
2022-12-12 10:20:07 -0500

    Add Page Tax to Properties

--- src/public/js/cdp-analytics.js
+++ src/public/js/cdp-analytics.js
@@ -158,8 +158,8 @@ if ( (typeof analytics !== 'undefined') && (cdp_analytics.taxonomy_context == "1
 	
 	const ADDWPTAX = function({ payload, next, integrations }) {
 		if (payload.obj.type == "page") {
-			if (cdp_analytics.categories != "") payload.obj.context.page.categories = cdp_analytics.categories;
-			if (cdp_analytics.tags != "") payload.obj.context.page.tags = cdp_analytics.tags;
+			if (cdp_analytics.categories != "") payload.obj.properties.categories = cdp_analytics.categories;
+			if (cdp_analytics.tags != "") payload.obj.properties.tags = cdp_analytics.tags;
 		}
 		next(payload);
 	};
