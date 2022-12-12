857baac71a5cb6620da7351c2200daf0cdb3c015
Chris Carrel <ccarrel@virtualhaven.net>
2022-12-12 09:57:36 -0500

    Fix Page call test

--- src/public/class-cdp-ajs.php
+++ src/public/class-cdp-ajs.php
@@ -75,10 +75,10 @@ class AJS {
             echo "<script>" . PHP_EOL;
             echo '!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://' . $subdomain . '/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="' . $writeKey . '";;analytics.SNIPPET_VERSION="4.15.3";';
             echo 'analytics.load("' . $writeKey . '");';
-            if (is_front_page() && ($this->_settings('cdp-page-home') !== "")) {
-                echo "analytics.page('" . $this->_settings('cdp-page-home') . "');";
-            } else if (is_404() && ($this->_settings('cdp-page-404') !== "")) {
-                echo "analytics.page('" . $this->_settings('cdp-page-404') . "');";
+            if (is_front_page() && ($segment['cdp-page-home'] !== "")) {
+                echo "analytics.page('" . $segment('cdp-page-home') . "');";
+            } else if (is_404() && ($segment['cdp-page-404'] !== "")) {
+                echo "analytics.page('" . $segment['cdp-page-404'] . "');";
             } else {
                 echo "analytics.page();";
             }
